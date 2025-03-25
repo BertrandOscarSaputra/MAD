a72228fb1644fa687d2efc98d352d35519738aa2
BertrandOscarSaputra <bertrandosc@gmail.com>
2025-03-25 15:52:36 +0800

    Exercise 7 Final

--- Exercise7.tsx
+++ Exercise7.tsx
@@ -1,15 +1,16 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {View, StyleSheet, Text, ScrollView} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Card from './components/userCard/index';
 
 const Exercise7 = () => {
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/users')
+      .get('https://reqres.in/api/users?per_page=12')
       .then(res => {
-        setUsers(res.data);
+        setUsers(res.data.data);
       })
       .catch(error => {
         console.log(error);
@@ -17,26 +18,26 @@ const Exercise7 = () => {
   }, []);
 
   return (
-    <View>
-      <Card
-        first="George"
-        last="Bluth"
-        avatar="https://reqres.in/img/faces/1-image.jpg"
-        email="george.bluth@reqres.in"
-      />
-      <Text>RestAPI Component</Text>
-      {users.map(user => {
-        return (
-          <View>
-            <Text>{user.name}</Text>
-            <Text>{user.email}</Text>
-          </View>
-        );
-      })}
-    </View>
+    <ScrollView>
+      <Text style={styles.title}>User List</Text>
+      {users.map(user => (
+        <Card
+          first={user.first_name}
+          last={user.last_name}
+          avatar={user.avatar}
+          email={user.email}
+        />
+      ))}
+    </ScrollView>
   );
 };
 
 export default Exercise7;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    margin: 30,
+    fontWeight: 'bold',
+  },
+});
