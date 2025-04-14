69c428528b44e82107b809da0d8ff7fa87bb472d
BertrandOscarSaputra <bertrandosc@gmail.com>
2025-04-14 15:38:35 +0800

    slicing sign in page

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 import {AppRegistry} from 'react-native';
-// import App from './App';
-// import Exercise2 from './Exercise2';
-// import {name as appName} from './app.json';
-// import Exercise3 from './Exercise3';
-// import {name as appName} from './app.json';
-// import Flexbox from './flexbox';
-// import {name as appName} from './app.json';
-// import Exercise4 from './Exercise4';
-// import {name as appName} from './app.json';
-// import SignIn from './signIn';
-// import {name as appName} from './app.json';
-// import Exercise6 from './Exercise6';
-// import {name as appName} from './app.json';
-import Exercise7 from './Exercise7';
+
+import App from './App';
 import {name as appName} from './app.json';
 
 // AppRegistry.registerComponent(appName, () => App);
 // AppRegistry.registerComponent(appName, () => Exercise2);
 // AppRegistry.registerComponent(appName, () => Exercise3);
-AppRegistry.registerComponent(appName, () => Exercise7);
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/svg.d.ts
@@ -0,0 +1,8 @@
+// declarations.d.ts
+declare module '*.svg' {
+  import {SvgProps} from 'react-native-svg';
+  import React from 'react';
+
+  const content: React.FC<SvgProps>;
+  export default content;
+}
